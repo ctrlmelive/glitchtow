de28786eb535e584f71ba78ff47d99873bf28fb2
ctrlmelive <118881107+ctrlmelive@users.noreply.github.com>
2023-03-13 00:43:55 -0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -3,6 +3,9 @@
 sleep 2m
 while [ "a" = "a" ]
 do
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
   curl https://oval-humane-pigment.glitch.me/
   curl https://aerial-quasar-study.glitch.me/
   sleep 7m
